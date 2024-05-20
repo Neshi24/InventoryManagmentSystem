068859314b75d0d09ea8e777c0fcb0c0855edff2
Filip <91468087+Neshi24@users.noreply.github.com>
2024-05-20 17:15:42 +0200

    Update MessageHandler.cs

--- projects/order-service/OrderService/RabbitMQ/MessageHandler.cs
+++ projects/order-service/OrderService/RabbitMQ/MessageHandler.cs
@@ -6,13 +6,21 @@ namespace OrderService.RabbitMQ;
 
 public class MessageHandler : BackgroundService
 {
+    private readonly IServiceProvider _serviceProvider;
+    public MessageHandler(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        
-            var connectionStr = "amqp://guest:guest@rabbitmq:5672/";
-                
-            var messageClient = new MessageClient(RabbitHutch.CreateBus(connectionStr));
+        using (var scope = _serviceProvider.CreateScope())
+        {
+            var itemService = scope.ServiceProvider.GetRequiredService<IOrderService>();
             
+            var connectionStr = "amqp://guest:guest@rabbitmq:5672/";
+
+            var messageClient = new MessageClient(RabbitHutch.CreateBus(connectionStr));
+
             messageClient.Listen<MessageIds>(
                 OnMessageReceived,
                 "missingItems"
@@ -23,19 +31,21 @@ public class MessageHandler : BackgroundService
                 try
                 {
                     Console.WriteLine($"Received message: OrderId = {messageIds.OrderId}, ItemsIds = [{string.Join(", ", messageIds.ItemsIds)}]");
-                    
+                    itemService.GetOrderById(messageIds.OrderId);
+
                 }
                 catch (Exception e)
                 {
                     Console.WriteLine(e.Message);
                 }
             }
-            
+
             while (!stoppingToken.IsCancellationRequested)
             {
                 Console.WriteLine("MessageHandler is listening for missing items.");
                 await Task.Delay(1000, stoppingToken);
             }
+        }
     }
         
 }
