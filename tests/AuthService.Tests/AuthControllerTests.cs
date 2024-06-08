187d5ef8f37c1d8de6e4fb073b503f63ab0d49bd
Kathurjan <kath097c@easv365.dk>
2024-06-08 18:04:59 +0200

    Feat: moved dbinmemory to different class.

--- tests/AuthService.Tests/AuthControllerTests.cs
+++ tests/AuthService.Tests/AuthControllerTests.cs
@@ -1,40 +1,9 @@
 ﻿using System.Net.Http.Json;
-using AuthService.Data;
 using AuthService.Model;
-using Microsoft.AspNetCore.Mvc.Testing;
+using AuthService.Tests.DBsetup;
 using FluentAssertions;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 
 
-public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup : class
-{
-    protected override void ConfigureWebHost(IWebHostBuilder builder)
-    {
-        builder.ConfigureServices(services =>
-        {
-           
-            var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<AuthDbContext>));
-            if (descriptor != null)
-            {
-                services.Remove(descriptor);
-            }
-
-            
-            services.AddDbContext<AuthDbContext>(options =>
-            {
-                options.UseInMemoryDatabase("InMemoryDbForTesting");
-            });
-
-            
-            var serviceProvider = services.BuildServiceProvider();
-            using var scope = serviceProvider.CreateScope();
-            var db = scope.ServiceProvider.GetRequiredService<AuthDbContext>();
-            db.Database.EnsureCreated();
-        });
-    }
-}
 
 public class AuthControllerTests : IClassFixture<CustomWebApplicationFactory<Program>>
 {
